e943f50e9d6c0694334ee0972f7308106c2e69d1
edwin <spark.com.co@gmail.com>
2020-03-22 11:19:30 -0500

    Set Layout component

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default function index() {
+  return (
+    <>
+      <h1>index works!</h1>
+      <Link to="/about">Go About</Link>
+    </>
+  );
+}
