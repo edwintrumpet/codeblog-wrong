19fa2eebc40637cfdfbc3452a6ab4f0474d14630
edwin <spark.com.co@gmail.com>
2020-03-22 17:02:26 -0500

    Set styled-components

---
+++ src/styles/index.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    .___gatsby {
+        position: absolute;
+    }
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    a {
+        text-decoration: none;
+    }
+`;
+
+export default GlobalStyles;
